022952e5e7847cc3152ef5c6a8202b6d80d6b2ce
kamilmessi <kamil.kamil0606@outlook.com>
2024-09-13 13:02:15 +0300

    added

--- lib/app_initialize.dart
+++ lib/app_initialize.dart
@@ -1,13 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:space_app/main.dart';
 import 'package:space_app/src/firebase/notification.dart';
 
 class AppInitializationService {
   static Future<void> initialize() async {
     WidgetsFlutterBinding.ensureInitialized();
-    // Load the user's preferred theme while the splash screen is displayed.
-    // This prevents a sudden theme change when the app is first displayed.
     await settingsController.loadSettings();
     await Firebase.initializeApp();
     NotificationService().init();
